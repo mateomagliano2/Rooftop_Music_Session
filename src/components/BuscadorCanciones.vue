60c7eaa2b54aec46d2553c953098612b8ab53909
Matias <matias.b.dev@gmail.com>
2023-07-01 12:25:05 -0300

    Se agrego codigo en el scrpt del componente

--- src/components/BuscadorCanciones.vue
+++ src/components/BuscadorCanciones.vue
@@ -4,10 +4,14 @@
   <img src="../assets/fondobuscador.jpg" class="card-img-top" >
   <div class="card-body">
     <h5 class="card-title">Buscador de artistas </h5>
-    <input type="text" placeholder="Ingresa tu artista o cancion... ">
+    <input v-model ="info" type="text" class="form-control-sm"  placeholder="Ingresa tu artista o cancion... ">
+
+
     <button type="submit" class="btn btn-primary">Buscar</button>
 
-    <p class="card-text">Encuentra tu artista favorito...crack.</p>
+    <p class="card-text">{{info}}Encuentra tu artista favorito...crack.</p>
+
+
     
   </div>
 </div>
@@ -16,6 +20,20 @@
 
 </template>
 <script>
+
+
+export default {
+    name: 'BuscadorCanciones',
+
+    data(){
+        return{
+            info: ""
+        }
+    },
+
+
+}
+
 </script>
 <style scoped>
 
